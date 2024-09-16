9e146824b9bd02233febb2247553755a92f7914b
rhyonomleni <93315228+rhyonomleni@users.noreply.github.com>
2024-09-17 01:00:42 +0700

    Hari 6 - Basic Gameplay

---
+++ Create With Code/Prototipe 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float jedaWaktu = 3;
+    private float waktu = 0;
+    void Start(){
+        waktu = jedaWaktu;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        waktu += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && waktu > jedaWaktu)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            waktu = 0;
+        }
+    }
+}
